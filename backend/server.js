42b9338c31a3752b294729bb40f2dd24eed5c595
NaijaKriz <imebachristianinnocent@gmail.com>
2025-04-21 17:27:10 +0100

    preparing my app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express  from 'express';
 import dotenv from "dotenv"
 import connectDB from './db/connectMongodb.js'
@@ -10,28 +11,34 @@ import notificationRoutes from './routes/notification.route.js';
 import {v2 as cloudinary} from 'cloudinary'
 
 dotenv.config()
-
-
-const app = express();
-
-const PORT = process.env.PORT || 5000
-
-app.use(express.json({limit: "5mb"}));
-app.use(express.urlencoded({extended:true}))
-
-app.use(cookieParser())
-
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
+const app = express();
+const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
+
+app.use(express.json({limit: "5mb"}));
+app.use(express.urlencoded({extended:true}))
+
+app.use(cookieParser())
+
+
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/notifications", notificationRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+    app.get("*", (req, res) =>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, (req, res) =>{
     console. log(`App is running on port ${PORT}`);
